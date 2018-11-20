76f5b21446453bbe677a1a28b7cb1a2132043b72
Brenton Otis <brenton.otis@gmail.com>
2018-11-20 09:03:29 -0600

    ArrayBag implementation

--- ArrayBag.java
+++ ArrayBag.java
@@ -11,6 +11,8 @@ public final class ArrayBag<T> implements BagInterface<T> {
 	private final T[] bag;
 	private int numberOfEntries;
 	private static final int DEFAULT_CAPACITY = 25;
+	private boolean integrityOK;
+	private static final int MAX_CAPACITY = 10000;
 	
 	/** Default constructor - creates empty bag with default/initial capacity of 25 */
 	public ArrayBag() {
@@ -20,11 +22,19 @@ public final class ArrayBag<T> implements BagInterface<T> {
 	/** Constructor; Creates empty bag with given initial capacity
 	 * @param desiredCapacity integer capacity desired */
 	public ArrayBag(int desiredCapacity) {
-		// Cast safe b/c null entries in new array
-		@SuppressWarnings("unchecked")
-		T[] tempBag = (T[])new Object[desiredCapacity]; // Unchecked cast
-		bag = tempBag;
-		numberOfEntries = 0;
+		integrityOK = false;
+		if (desiredCapacity <= MAX_CAPACITY) {
+			// Cast safe b/c null entries in new array
+			@SuppressWarnings("unchecked")
+			T[] tempBag = (T[])new Object[desiredCapacity]; // Unchecked cast
+			bag = tempBag;
+			numberOfEntries = 0;
+			integrityOK = true;
+		}
+		else {
+			throw new IllegalStateException("Attempt to create a bag whose " +
+											"capacity exceeds allowed maximum.");
+		}	
 	}
 
 	@Override
@@ -40,7 +50,11 @@ public final class ArrayBag<T> implements BagInterface<T> {
 	}
 
 	@Override
+	/** Adds new entry to bag
+	 * @param newEntry object being added as new entry
+	 * True if addition is successful, otherwise false */
 	public boolean add(T newEntry) {
+		checkIntegrity();
 		boolean result = true;
 		if (isArrayFull()) {
 			result = false;			
@@ -49,9 +63,10 @@ public final class ArrayBag<T> implements BagInterface<T> {
 			bag[numberOfEntries] = newEntry;
 			numberOfEntries++;
 		}
-		return false;
+		return result;
 	}
 	
+	// returns true if max num of entries exceeds length of Bag, otherwise false
 	private boolean isArrayFull() {
 		return numberOfEntries >= bag.length;
 	}
@@ -97,5 +112,11 @@ public final class ArrayBag<T> implements BagInterface<T> {
 		}
 		return result;
 	}
+	
+	// Throws exception if object not initialized
+	private void checkIntegrity() {
+		if (!integrityOK) {
+			throw new SecurityException("ArrayBag object is corrupt.");
+		}
+	}
 }
-
