98ca9b7a4add05e70ece6df7a6cb5cbc737de713
Brenton Otis <brenton.otis@gmail.com>
2018-12-10 23:41:00 -0600

    Program that evaluates postfix expression (via stack)

--- PostfixCalculator.java
+++ PostfixCalculator.java
@@ -1,9 +1,9 @@
-package stacks2;
+package stacks3;
 
 /**
  * Program that reads postfix expression and evaluates/returns result
+ * Takes in 5 data points for use in two pre-determined expressions per assignment spec.
  * @author Brenton Otis
- * --> throws EmptyStackException when evaluating second scenario; Need to fix
  */
 
 import java.util.*;
@@ -12,49 +12,92 @@ public class PostfixCalculator {
 
 	public static int evaluatePostfix(String postfix) {
 		
-		// Initialize new/empty stack
-		Stack<Integer> valueStack = new Stack<>();
-		
-		// Read through all characters
-		for(int i = 0; i < postfix.length(); ++i) {
-			char character = postfix.charAt(i);
-			// Ignore spaces
-			if(character == ' ') continue;
+		// try/catch to catch EmptyStackException
+		try {
+			Stack<Integer> valueStack = new Stack<Integer>();
+			int characterCount = postfix.length();
+			int index = 0;
+			char nextChar = ' ';
+			int operandTwo, operandOne, result;
 			
-			// If character is operand, push to stack
-			if(Character.isDigit(character)) {
-				valueStack.push(character - '0');
-			}
-			// If character is operator, pop most recent two elements from stack/apply operator
-			else {
-				int value1 = valueStack.pop();
-				int value2 = valueStack.pop();
-				// switch statement evaluates operands based on operator, pushes result to stack
-				switch(character) {
-				case '+':
-					valueStack.push(value2+value1);
-					break;
-				case '-':
-					valueStack.push(value2-value1);
-					break;
-				case '/':
-					valueStack.push(value2/value1);
-					break;
-				case '*':
-					valueStack.push(value2*value1);
-					break;
+			// read through all characters in expression, assign to nextChar variable
+			for (index = 0; index < characterCount; index++){
+				nextChar = postfix.charAt(index);
+				
+				// if character is operand, push to stack
+				if (Character.isDigit(nextChar)) {
+					String s = Character.toString(nextChar);
+					valueStack.push(Integer.parseInt(s));
+				}
+				// else if statements evaluate operators
+				// pop most recent values/operands from stack & apply operator
+				// push result back to stack
+				else if (nextChar=='+') {     
+					operandTwo = valueStack.pop();
+					operandOne = valueStack.pop();
+					result = operandOne + operandTwo;
+					valueStack.push((Integer) result);
+				}
+				else if (nextChar =='-') {
+					operandTwo = valueStack.pop();
+					operandOne = valueStack.pop();
+					result = operandOne - operandTwo;
+					valueStack.push((Integer) result);
+				}
+				else if (nextChar=='*') {
+					operandTwo = valueStack.pop();
+					operandOne = valueStack.pop();
+					result = operandOne * operandTwo;
+					valueStack.push((Integer) result);
+				}
+				else if (nextChar=='/') {
+					operandTwo = valueStack.pop();
+					operandOne = valueStack.pop();
+					result = operandOne / operandTwo;
+					valueStack.push((Integer) result);
+				}
+
+				else {
+					return 0;
 				}
 			}
+			return valueStack.peek();  
 		}
-		return valueStack.pop();	
+		catch(EmptyStackException e) {
+			return -1; 
+		}
 	}
 	
-	public static void main (String[] args) {
+	public static void main(String[] args) {
+
+		@SuppressWarnings("resource")
+		Scanner scnr = new Scanner(System.in);
 		
-		String postfix1 = "12+4*5-";
-		System.out.println("Postfix 12+4*5- evaluates to: " +evaluatePostfix(postfix1));
+		// ask user for five integer values, assign to variables
+		System.out.println("Please enter your first integer: ");
+		int data1= Integer.parseInt(scnr.nextLine());
+		System.out.println("Please enter your second integer: ");
+		int data2= Integer.parseInt(scnr.nextLine());
+		System.out.println("Please enter your third integer: ");
+		int data3= Integer.parseInt(scnr.nextLine());
+		System.out.println("Please enter your fourth integer: ");
+		int data4= Integer.parseInt(scnr.nextLine());
+		System.out.println("Please enter your fifth integer: ");
+		int data5= Integer.parseInt(scnr.nextLine());
 		
-		String postfix2 = "12*4*1-/53*+";
-		System.out.println("Postfix 12*4*1-/53*+ evaluates to: " +evaluatePostfix(postfix2));
+		// builds string/expression of data values in line with assignment spec
+		// data1 data2 + data3 * data4 -
+		String expression1 = String.valueOf(data1) + String.valueOf(data2) + "+" + String.valueOf(data3) + "*"
+			+ String.valueOf(data4) + "-";
+		
+		// prints expression + evaluation via evaluatePostfix method
+		System.out.println("data1 data2 + data3 * data4 - evaluates to: "+ evaluatePostfix(expression1));
+		
+		// builds data1 data2 * data3 * data1 -/ data4 data5 *+
+		String expression2 = String.valueOf(data1) + String.valueOf(data2) + "*" + String.valueOf(data3) + "*"
+			+ String.valueOf(data1) + "-" + "/" + String.valueOf(data4) + String.valueOf(data5) + "*" + "+";
+		
+		// prints expression + evaluation via evaluatePostifix method
+		System.out.println("data1 data2 * data3 * data1 - / data4 data5 * + evaluates to: " + evaluatePostfix(expression2));
 	}
 }
