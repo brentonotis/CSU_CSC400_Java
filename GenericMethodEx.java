889395d875d3d3f8f24e219ef8ec1de8f7e5e91e
Brenton Otis <brenton.otis@gmail.com>
2018-12-18 21:50:18 -0600

    example of generic methods, bounded type params & wildcard

--- GenericMethodEx.java
+++ GenericMethodEx.java
@@ -1,12 +1,13 @@
 package generics;
 
 /**
- * Example of a generic method (<T> displayArray)
+ * Example of generic methods, bounded type parameters & a wildcard
  * @author Brenton Otis
  */
 
 public class GenericMethodEx {
-
+	
+	// displays array of generic type (<T>)
 	public static <T> void displayArray(T[] myArray) {
 		
 		for (T arrayEntry : myArray) {
@@ -14,6 +15,20 @@ public class GenericMethodEx {
 			System.out.print(" ");
 		} // end for
 		System.out.println(); // end displayArray
+	}
+	
+	// <T extends Comparable <T>> bounds T to represent class types that provide the compareTo method
+	// '?' represents any class type, '? super T' means any superclass of T
+	// '? super T' allows comparisons to objects of superclass of T (instead of only explicit objects of T)
+	public static <T extends Comparable<? super T>> void arrayMinimum(T[] anArray) {
+		
+		T minimum = anArray[0];
+		for (T arrayEntry: anArray) {
+			if (arrayEntry.compareTo(minimum) < 0) {
+				minimum = arrayEntry;
+			}
+		}
+		return;
 	}
 	
 	// driver
